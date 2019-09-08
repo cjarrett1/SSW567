477aee11fae090792c8592cfeebf111bfcf71df7
Craig <craig.m.jarrett@gmail.com>
2019-09-07 20:01:25 -0400

    Update to TestTriangle.py
    Updating the test values

--- HW02A_CraigJarrett/TestTriangle.py
+++ HW02A_CraigJarrett/TestTriangle.py
@@ -18,28 +18,28 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangle(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3,4,5),'Right')
         
     def testIsoceles(self): 
-        self.assertEqual(classifyTriangle(1,2,3),'Isoceles','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(5,5,7),'Isoceles)
 
     def testScalene(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Scalene','5,3,4 is a Scalene triangle')
+        self.assertEqual(classifyTriangle(3,4,5),'Scalene')
         
     def testEquilateral(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral')
     
     def testIntegers(self):
-        self.assertEqual(classifyTriangle(-1,-2,-3), 'InvalidInput', 'Some or all of the values are not integers')
+        self.assertEqual(classifyTriangle(-1,-2,-3), 'InvalidInput')
         
     def testValidTriangleA(self):
-        self.assertEqual(classifyTriangle(10,10,5), 'NotATriangle','This is not a triangle')
+        self.assertEqual(classifyTriangle(1,1,10), 'NotATriangle')
         
     def testValidTriangleB(self):
-        self.assertEqual(classifyTriangle(0,10,0), 'InvalidInput','Some or all of the values are not valid inputs')
+        self.assertEqual(classifyTriangle(0,10,0), 'InvalidInput')
         
     def testValidTriangleC(self):
-        self.assertEqual(classifyTriangle(300,500,250), 'InvalidInput','Some or all of the values are not valid inputs')
+        self.assertEqual(classifyTriangle(300,500,250), 'InvalidInput')
 
 if __name__ == '__main__':
     print('Running unit tests...')
